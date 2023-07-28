6eb706238152512192793be2ea7d449de8593ff9
Kaito <hoangdai.d93@gmail.com>
2023-07-28 13:24:44 +0200

    add @Enumerated annotation

--- backend/src/main/java/com/dreamteam/app/entities/Music.java
+++ backend/src/main/java/com/dreamteam/app/entities/Music.java
@@ -1,12 +1,9 @@
 package com.dreamteam.app.entities;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.Set;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,24 +16,23 @@ public class Music {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+
 	private String name;
-	private String artiste;
+	private String artist;
 	private String duration;
 	private LocalDate releasedAt;
-	private List<Tags> tags;
+
+	@Enumerated(EnumType.STRING)
+	private Set<Tags> tags;
 	
-	
-	public Music(long id, String name, String artiste, String duration, LocalDate releasedAt, List<Tags> tags) {
+	public Music(long id, String name, String artist, String duration, LocalDate releasedAt, Set<Tags> tags) {
 		this.id = id;
 		this.name = name;
-		this.artiste = artiste;
+		this.artist = artist;
 		this.duration = duration;
 		this.releasedAt = releasedAt;
 		this.tags = tags;
 	}
-	public Music() {
+	public Music() {}
 
-	}
-	
-	
 }
