d9feee6f1588a907c685359702ba0fb360d82dde
Kaito <hoangdai.d93@gmail.com>
2023-08-05 18:38:36 +0200

    fix: add Lombok annotations, modify update()

--- backend/src/main/java/com/dreamteam/app/controllers/MusicController.java
+++ backend/src/main/java/com/dreamteam/app/controllers/MusicController.java
@@ -1,8 +1,10 @@
 package com.dreamteam.app.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.dreamteam.app.dto.MusicDTO;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import com.dreamteam.app.services.MusicService;
@@ -10,12 +12,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/music")
+@AllArgsConstructor
 public class MusicController {
-	private MusicService service;
-
-	public MusicController(MusicService service) {
-		this.service = service;
-	}
+	private final MusicService service;
 
 	@GetMapping
 	public List<MusicDTO> findAll(){
@@ -38,7 +37,17 @@ public class MusicController {
 		@RequestPart("imgFile") MultipartFile imgFile,
 		@RequestPart("audioFile") MultipartFile audioFile
 	) {
-		return service.add(musicDTO, imgFile, audioFile);
+		return service.add(musicDTO, imgFile, audioFile, null);
 	}
-	
+
+	@PostMapping("/update/{id}")
+	public MusicDTO update(
+		@PathVariable Optional<Long> id,
+		@RequestPart("fileUpload") MusicDTO musicDTO,
+		@RequestPart("imgFile") MultipartFile imgFile,
+		@RequestPart("audioFile") MultipartFile audioFile
+	){
+		System.out.println(id);
+		return service.add(musicDTO, imgFile, audioFile, id);
+	}
 }
