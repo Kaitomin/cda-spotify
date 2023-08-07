8936b8f191bcb302788caa78356d3a28efd05bf0
Ambre <guichard.ambr@gmail.com>
2023-08-07 11:16:33 +0200

    feat form add music

--- cda-spotify-front/src/components/FormMusic.jsx
+++ cda-spotify-front/src/components/FormMusic.jsx
@@ -1,35 +1,74 @@
+import { useState } from 'react'
 import '../style.css'
+import React from 'react'
+import {useForm} from 'react-hook-form'
 
 const FormMusic = () => {
-  return (
+    const [input, setInputs] = useState({})
+    const {handleSubmit, register, formState : {errors}} = useForm()
     
-    <form className='form'>
+    function onSubmit (data) {
+        console.log(data)
+    }
+  
+    return (
+    
+    <form className='form' onSubmit={handleSubmit(onSubmit)}>
         <h1>Enregistrer une nouvelle musique</h1>
-        <label className='label'>
+        <label className='label' >
             Titre de la musique :
-            <input type="text" name="title" className='input'/>
+            <input type="text" 
+                name="title" 
+                className='input' 
+                {...register("title")}
+            />
         </label>
         <label className='label'>
             Artiste de la musique :
-            <input type="text" name="artist" className='input'/>
+            <input 
+                type="text" 
+                name="artist" 
+                className='input'
+                {...register("artist")}
+                />
         </label>
         <label className='label'>
             Durée de la musique : 
-            <input type="text" name="duration" className='input'/>
+            <input 
+                type="text" 
+                name="duration" 
+                className='input'
+                {...register("duration")}
+                />
         </label>
         <label className='label'>
             Date de sortie de la musique : 
-            <input type="datetime-local" name="releasedAt" className='input'/>
+            <input 
+                type="datetime-local" 
+                name="releasedAt" 
+                className='input'
+                {...register("releasedAt")}
+                />
         </label>
         <label className='label'>
             Image associée :
-            <input type="file" name="imgUri" className='input'/>
+            <input 
+                type="file" 
+                name="imgUri" 
+                className='input'
+                {...register("imgUri")} 
+                />
         </label>
         <label className='label'>
             Fichier de la musique :
-            <input type="file" name="audioUri" className='input'/>
+            <input 
+                type="file" 
+                name="audioUri" 
+                className='input'
+                {...register("audioUri")}
+                />
         </label>
-        <h2>Tag de la musique :</h2>
+        <label className='label'>Tag de la musique :
         <label className='checkbox'>
             Pop
             <input type="checkbox" name="pop" className='input'/>
@@ -46,9 +85,10 @@ const FormMusic = () => {
             Rap
             <input type="checkbox" name="rap" className='input'/>
         </label>
-    
-        <input type="button" name="submit" placeholder='Submit'/>
-
+    </label>
+    <label className='label'>
+        <input type="submit" name="submit" value="Valider la musique"/>
+    </label>
 
     </form>
 
