38182d3e98ad7cbaa5f3eea64e7e5af22030e755
Kaito <hoangdai.d93@gmail.com>
2023-08-09 11:18:40 +0200

    fix: handleSubmit() function to send correct formData

--- cda-spotify-front/src/components/FormMusic.jsx
+++ cda-spotify-front/src/components/FormMusic.jsx
@@ -2,61 +2,60 @@ import { useState } from 'react'
 import '../style.css'
 import React from 'react'
 import axios from 'axios'
-import {useForm} from 'react-hook-form'
 
 const FormMusic = () => {
-    const {handleSubmit, register, formState : {errors}} = useForm()
     const baseUrl = "http://localhost:8080/api/music/new";
-    const {post, setPost} = useState();
-    
-    let objFormData = new FormData();
-    /*React.useEffect(() => {
-        axios.get().then((response) => {
-            register.data
-        })
-    })*/
-   //axios.get(baseUrl).then((data) => console.log(data))
-
-    function createPost () {
-        axios.post(baseUrl,
-                objFormData
-        )
-        .then((response) => {
-            console.log("boop" + response)
-        }).catch((exception) => {
-            console.log(exception)
-        })
+    const [music, setMusic] = useState({
+        title: "",
+        artist: "",
+        duration: "",
+        releasedAt: "",
+        tags: ["POP"],
+        imgFile: null,
+        audioFile: null
+    })
+    const handleChange = e => {
+        let {name, value, files} = e.target
+        
+        e.target.type == 'file' ?
+        setMusic({...music, [name]: files[0]}) :
+        setMusic({...music, [name]: value})
     }
 
-    function onSubmit (data) {
+    const handleSubmit = e => {
+        e.preventDefault()
 
-        let obj  = {
-            "title" : data.title,
-            "artist" : data.artist,
-            "duration" : data.duration,
-            "releasedAt" : "2014-05-25T08:20:03",
-            "tags" : ["POP", "ROCK"]};
+        const formData = new FormData()
+
+        const newMusic  = {
+            "title" : music.title,
+            "artist" : music.artist,
+            "duration" : music.duration,
+            "releasedAt" : music.releasedAt,
+            "tags" : music.tags
+        }
         
-       objFormData.append("fileUpload", JSON.stringify(obj));
-       objFormData.append("imgFile", data.imgUri);
-       objFormData.append("audioFile", data.audioUri);
-       console.log(objFormData);
-       return objFormData;
-        
+        formData.append("fileUpload", 
+            new Blob([JSON.stringify(newMusic)], {
+                type: "application/json"
+            }))
+        formData.append("imgFile", music.imgFile);
+        formData.append("audioFile", music.audioFile);
+
+        axios.post(baseUrl, formData)
+            .then(response => console.log(response.data))
+            .catch(exception => console.log(exception))        
     }
-
-    // 3 champs dans un formdata : un obj avec les champs txt / img / son 
   
-    return (
-    
-    <form className='form' onSubmit={handleSubmit(onSubmit)}>
+    return (    
+    <form className='form' onSubmit={handleSubmit}>
         <h1>Enregistrer une nouvelle musique</h1>
         <label className='label' >
             Titre de la musique :
             <input type="text" 
                 name="title" 
                 className='input' 
-                {...register("title")}
+                onChange={handleChange}
             />
         </label>
         <label className='label'>
@@ -65,8 +64,8 @@ const FormMusic = () => {
                 type="text" 
                 name="artist" 
                 className='input'
-                {...register("artist")}
-                />
+                onChange={handleChange}
+            />
         </label>
         <label className='label'>
             Durée de la musique : 
@@ -74,8 +73,8 @@ const FormMusic = () => {
                 type="text" 
                 name="duration" 
                 className='input'
-                {...register("duration")}
-                />
+                onChange={handleChange}
+            />
         </label>
         <label className='label'>
             Date de sortie de la musique : 
@@ -83,56 +82,50 @@ const FormMusic = () => {
                 type="datetime-local" 
                 name="releasedAt" 
                 className='input'
-                {...register("releasedAt")}
-                />
+                onChange={handleChange}
+            />
         </label>
         <label className='label'>
             Image associée :
             <input 
                 type="file" 
-                name="imgUri" 
+                name="imgFile" 
                 className='input'
-                {...register("imgUri")} 
-                />
+                onChange={handleChange}
+            />
         </label>
         <label className='label'>
             Fichier de la musique :
             <input 
                 type="file" 
-                name="audioUri" 
+                name="audioFile" 
                 className='input'
-                {...register("audioUri")}
-                />
+                onChange={handleChange}
+            />
         </label>
         <label className='label'>Tag de la musique :
-        <label className='checkbox'>
-            Pop
-            <input type="checkbox" name="pop" className='input' {...register("checkbox")}/>
+            <label className='checkbox'>
+                Pop
+                <input type="checkbox" name="pop" className='input' />
+            </label>
+            <label className='checkbox'>
+                Rock
+                <input type="checkbox" name="rock" className='input' />
+            </label>
+            <label className='checkbox'>
+                AdrienLeBest
+                <input type="checkbox" name="adrien" className='input' />
+            </label>
+            <label className='checkbox'>
+                Rap
+                <input type="checkbox" name="rap" className='input' />
+            </label>
         </label>
-        <label className='checkbox'>
-            Rock
-            <input type="checkbox" name="rock" className='input' {...register("checkbox")}/>
+        <label className='label'>
+            <input type="submit" name="submit" value="Valider la musique" />
         </label>
-        <label className='checkbox'>
-            AdrienLeBest
-            <input type="checkbox" name="adrien" className='input' {...register("checkbox")}/>
-        </label>
-        <label className='checkbox'>
-            Rap
-            <input type="checkbox" name="rap" className='input' {...register("checkbox")}/>
-        </label>
-    </label>
-    <label className='label'>
-        <input type="submit" name="submit" value="Valider la musique" onClick={createPost()}/>
-    </label>
-
     </form>
-
-
-
-
   )
 }
 
 export default FormMusic
-//
