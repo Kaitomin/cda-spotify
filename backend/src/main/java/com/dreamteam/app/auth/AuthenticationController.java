e27f932880aec318ec00aac142e83b733171002c
Kaito <hoangdai.d93@gmail.com>
2023-08-10 11:38:55 +0200

    feat: authentication & authorization with jwt token

---
+++ backend/src/main/java/com/dreamteam/app/auth/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.dreamteam.app.auth;
+
+import com.dreamteam.app.dto.UserDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody UserDTO req) {
+        return ResponseEntity.ok(service.register(req));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest req
+    ) {
+        return ResponseEntity.ok(service.authenticate(req));
+    }
+}
---
+++ backend/src/main/java/com/dreamteam/app/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.dreamteam.app.config;
+
+import com.dreamteam.app.jwt.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/auth/**", "/api/**").permitAll()
+                        //.anyRequest().authenticated()
+                )
+                /*.formLogin(formLogin -> formLogin
+                    .loginPage("http://localhost:5173/login")
+                    .permitAll()
+                )*/
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        //.rememberMe(Customizer.withDefaults());
+        return http.build();
+    }
+}
