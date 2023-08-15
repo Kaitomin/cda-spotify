d285d3b3b1a4dede934dfbec28f4414bfd06101c
Kaito <hoangdai.d93@gmail.com>
2023-08-15 22:30:06 +0200

    fix: hibernate.ddl: update, disable auto storage deletion

--- backend/src/main/java/com/dreamteam/app/AppApplication.java
+++ backend/src/main/java/com/dreamteam/app/AppApplication.java
@@ -18,7 +18,7 @@ public class AppApplication {
 	@Bean
 	CommandLineRunner init(StorageService storageService) {
 		return (args) -> {
-			storageService.deleteAll();
+			//storageService.deleteAll();
 			storageService.init();
 		};
 	}
