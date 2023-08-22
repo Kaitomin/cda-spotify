636c039360040a092651d26e733b2f7e0fb7d9fe
Kaito <hoangdai.d93@gmail.com>
2023-08-22 10:01:49 +0200

    fix: secondary nav display fix

--- cda-spotify-front/src/components/SecondaryNav.jsx
+++ cda-spotify-front/src/components/SecondaryNav.jsx
@@ -4,14 +4,18 @@ import useAuth from '../hook/useAuth'
 
 const SecondaryNav = () => {
   const { currentUser, logout } = useAuth()
+  const isAuthenticated = localStorage.getItem('isAuthenticated')
+  const isAdmin = localStorage.getItem('isAdmin')
 
   return (
     <nav className='secondary-nav position-fixed bottom-0 w-100 border-top border-1'>
       <ul className='d-flex justify-content-around d-sm-none m-0 py-3 px-0'>
         <Link to="/"><i className="fa-solid fa-house"></i></Link>
         <Link to="/search"><i className="fa-solid fa-magnifying-glass"></i></Link>
-        <Link to="/account"><i className="fa-solid fa-user"></i></Link>
-        { currentUser.role === "ADMIN" &&
+        { isAuthenticated &&
+          <Link to="/account"><i className="fa-solid fa-user"></i></Link>
+        }
+        { isAuthenticated && isAdmin == 'true' &&
           <Link to="/dashboard"><i className="fa-solid fa-gauge"></i></Link>
         }
       </ul>
